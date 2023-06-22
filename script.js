798c66178a76fa793f07d6fb07d68c8068973d8d
ducdanh2304 <119092330+ducdanh2304@users.noreply.github.com>
2023-06-22 22:09:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const formOpenBtn = document.querySelector("#form-open"),
+  home_container = document.querySelector(".container"),
+  home_home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+
+formOpenBtn.addEventListener("click", () => home_container.classList.add("show"));
+formOpenBtn.addEventListener("click", () => home_home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home_container.classList.remove("show"));
+formCloseBtn.addEventListener("click", () => home_home.classList.remove("show"));
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
+
+// TYPING LETTERS
